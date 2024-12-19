caf0df5f775c174993f0ca498a99ddbd4f8bd739
khazarglzd <khazarguluzade@gmail.com>
2024-12-19 16:23:18 +0400

    adding dependencies & Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 
 import './App.css'
+import Search from './components/search/Search.jsx'
 
 function App() {
 
+  const handleOnSearchChange = (searchData) => {
+    console.log("weather data")
+  }
 
   return (
-    <>
-      Weather App
-    </>
+    <div className='container'>
+      <Search onSearchChange={handleOnSearchChange} />
+    </div>
+
   )
 }
 
