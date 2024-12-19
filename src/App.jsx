e01477098e1b690c4baee2dbbb81ac6506c154e3
khazarglzd <khazarguluzade@gmail.com>
2024-12-19 17:11:30 +0400

    Revert "adding dependencies & Search component"
    This reverts commit caf0df5f775c174993f0ca498a99ddbd4f8bd739.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 
 import './App.css'
-import Search from './components/search/Search.jsx'
 
 function App() {
 
-  const handleOnSearchChange = (searchData) => {
-    console.log("weather data")
-  }
 
   return (
-    <div className='container'>
-      <Search onSearchChange={handleOnSearchChange} />
-    </div>
-
+    <>
+      Weather App
+    </>
   )
 }
 
